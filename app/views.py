de7251a81d56444409a825b4fd52c75b9156ff02
htygithub <tyhuang@mail.ntust.edu.tw>
2016-06-13 15:59:53 +0800

    update app

--- app/views.py
+++ app/views.py
@@ -3,23 +3,21 @@ Definition of views.
 """
 
 from django.shortcuts import render
-from django.shortcuts import render_to_response
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.template import RequestContext
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
 
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
-
     return render(
         request,
-        'app/index.html',
+        #'app/index.html',
+        'app/fitting_input.html',
         context_instance = RequestContext(request,
         {
-            'title':'Home Page',
+            'title':'Property Value Prediction',
             'year':datetime.now().year,
         })
     )
@@ -33,7 +31,7 @@ def contact(request):
         context_instance = RequestContext(request,
         {
             'title':'Contact',
-            'message':'Your contact page.',
+            #'message':'Your contact page.',
             'year':datetime.now().year,
         })
     )
@@ -47,216 +45,194 @@ def about(request):
         context_instance = RequestContext(request,
         {
             'title':'About',
-            'message':'Your application description page.',
+            'message':'This is a website providing you the prediction of your property value.',
             'year':datetime.now().year,
         })
     )
 
 
-
-def T1_input(request):
-    from app.forms import BootstrapCurveFittingForm
-        
-    return render(
-        request,
-        'app/inp_T1.html',
-        context_instance = RequestContext(request,
-        { 
-            'title':'T1 Fitting',
-            'form': BootstrapCurveFittingForm,
-               })
-    )
-
-
-def T1_result(request):
-    import pyFitMR.T1_lib as T1_lib
-    
-    t_value = request.POST.get('t_value')
-    y_value = request.POST.get('y_value')
-    fitted_result_dict = T1_lib.T1fit(t_value, y_value)
-    return render(
-        request,
-        'app/resultpage.html',
-        context_instance = RequestContext(request, fitted_result_dict)
-       )
-
-
+from app.forms import BootstrapCurveFittingForm
 def T1LL_input(request):
-    from app.forms import BootstrapCurveFittingForm
-        
+
     return render(
         request,
-        'app/inp_T1LL.html',
+        'app/fitting_input.html',
         context_instance = RequestContext(request,
-        { 
-            'title':'Look-Locker T1 Fitting',
-            'form': BootstrapCurveFittingForm,
+        {
+            'title':'Fitting Input',
+            'form': BootstrapCurveFittingForm
                })
     )
 
 
 def T1LL_result(request):
-    import pyFitMR.T1LL_lib as T1LL_lib
-    
-    t_value = request.POST.get('t_value')
-    y_value = request.POST.get('y_value')
-    fitted_result_dict = T1LL_lib.T1LLfit(t_value, y_value)
+    crime=request.POST.get('crime')
+    zn=request.POST.get('zn')
+    inidus=request.POST.get('inidus')
+    nox=request.POST.get('nox')
+    rm=request.POST.get('rm')
+    age=request.POST.get('age')
+    dis=request.POST.get('dis')
+    rad=request.POST.get('rad')
+    tax=request.POST.get('tax')
+    ptratio=request.POST.get('ptratio')
+    Bk=request.POST.get('Bk')
+    lstat=request.POST.get('lstat')
+    optradio=request.POST.get('optradio')
+
+    MEDV_linear = linear_fitting(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat)
+    pic_linear = linear_plot(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat)
+    MEDV_SVR = SVR_fitting(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat)
+    pic_SVR = SVR_plot(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat)
+    script, div = bokeh_plot(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat)
+
+    result_dict={
+        "MEDV_linear":MEDV_linear,
+        "pic_linear":pic_linear,
+        "MEDV_SVR":MEDV_SVR,
+        "pic_SVR":pic_SVR,
+        "the_script":script,
+        "the_div":div
+    }
     return render(
-        request,
-        'app/resultpage.html',
-        context_instance = RequestContext(request, fitted_result_dict)
-       )
+    request,
+    'app/boston_result.html',
+    context_instance = RequestContext(request, result_dict)       )
+    #return HttpResponse(y)
 
-def T2_input(request):
-    from app.forms import T2Form
-         
-    return render(
-        request,
-        'app/inp_T2.html',
-        context_instance = RequestContext(request, 
-		{'title':'T2 Fitting Input',
-	            'form': T2Form}))		
-
-def T2_result(request):
-    
-    import pyFitMR.T2_lib as T2_lib
-    
-    t_value = request.POST.get('t_value')
-    y_value = request.POST.get('y_value')
-    fitted_result_dict = T2_lib.T2fit(t_value, y_value)  
-    
-    return render(
-        request,
-
-        'app/resultpage.html',
-        context_instance = RequestContext(request, fitted_result_dict)
-    )
-
-
-
-def AHA17_input(request):
-    from app.forms import AHAForm        
-    return render(
-        request,
-        'app/inp_AHA17.html',
-        context_instance = RequestContext(request, 
-        {'title':'AHA 17 Input',
-                'form': AHAForm}))      
-
-def AHA17_result(request):
-    import pyFitMR.AHA17_lib as AHA17_lib
-    
-    t_value = request.POST.get('t_value')
-  
-    result_dict=AHA17_lib.Plot17(t_value)
-
-    return render(request,'app/resultpage.html',
-        context_instance = RequestContext(request, result_dict)
-       )
-	   
-def perfusion_input(request):
-    from app.forms import perfusionForm
-         
-    return render(
-        request,
-        'app/inp_perfusion.html',
-        context_instance = RequestContext(request, 
-		{'title':'perfusion Fitting Input',
-	            'form': perfusionForm}))		
-
-def perfusion_result(request):
-    
-    import pyFitMR.perfusion_lib as perfusion_lib
-    
-    t_value = request.POST.get('t_value')
-    y_value = request.POST.get('y_value')
-    fitted_result_dict = perfusion_lib.perfusionfit(t_value, y_value)  
-    
-    return render(
-        request,
-
-        'app/resultpage.html',
-        context_instance = RequestContext(request, fitted_result_dict)
-    )
-
-
-def fcmat_input(request):
-    from app.models import Document
-    from app.forms import DocumentForm
-    from django.http import HttpResponseRedirect
-    from django.core.urlresolvers import reverse
-    from django.conf import settings #or from my_project import settings
+def linear_fitting(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat):
+    from django.conf import settings
     import os
-    # Handle file upload
-    #print 
-    
-    form = DocumentForm() # A empty, unbound form
+    import numpy as np
+    from sklearn.externals import joblib
 
-    # Load documents for the list page
-    #documents = Document.objects.all()
+    lr=joblib.load(os.path.join(settings.PROJECT_ROOT,'app','lrmachine.pkl'))
 
-    # Render list page with the documents and the form
-    return render_to_response(
-        'app/inp_fcmat.html',
-        {'title':'Displaying Functional Connectivity Matrix','form': form},
-        context_instance=RequestContext(request)
-    )
-    
-
-def fcmat_result(request):
-    from app.models import Document
-    from app.forms import DocumentForm
-    from django.http import HttpResponseRedirect
-    from django.core.urlresolvers import reverse
-    from django.conf import settings #or from my_project import settings
-    import os
-    import pyFitMR.fcmat_lib as fcmat_lib
-    from django.conf import settings #or from my_project import settings
-    uploadedfile = False
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            newdoc = Document(docfile = request.FILES['docfile'])
-            newdoc.save()
-            matfile = os.path.join(settings.MEDIA_ROOT,newdoc.docfile.name)
-            uploadedfile = True
-        else:
-            FILE_ROOT = os.path.abspath(os.path.dirname(__file__))
-            matfile = os.path.join(FILE_ROOT,'CC_testfile.mat')
+    if not crime:
+        my_variable =[0.02729,0,7.07,0,0.469,7.185,61.1,4.9671,2,242,17.8,39.283,9.14]
+        Y=lr.predict(np.array(my_variable))
     else:
-        #form = DocumentForm() # A empty, unbound form
-        return HttpResponseRedirect(reverse('fcmat_input'))
+        x = np.array([crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat], dtype=np.float64)
+        Y=lr.predict(x)
 
-    import scipy.io
-    #import matplotlib.pyplot as plt
-    mat = scipy.io.loadmat(matfile)
-    FC=mat['connectome']
-    #FC=mat.items()[0][1]
-    #plt.imshow(FC)
-    #print FC
-    script, div = fcmat_lib.plot(FC)
-    if uploadedfile:
-        newdoc.docfile.delete()
-    return render(request, 'app/fcmat_result.html', {"the_script":script, "the_div":div})
+    return Y
+
+def linear_plot(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat):
+    from django.conf import settings
+    import os, matplotlib.pyplot as plt
+    from sklearn.externals import joblib
+    try:
+        from StringIO import StringIO
+    except ImportError:
+        from io import StringIO
+    from sklearn import datasets
+    from sklearn.cross_validation import cross_val_predict
+    import numpy as np
+
+    lr=joblib.load(os.path.join(settings.PROJECT_ROOT,'app','lrmachine.pkl'))
+    boston = datasets.load_boston()
+    y = boston.target
+    Y=linear_fitting(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat)
+
+    try:
+        predicted = cross_val_predict(lr, boston.data, y, cv=10)
+        predict_y=Y
+
+        plt.figure(1)
+        plt.clf()
+        plt.scatter(predicted,y,s=2)
+        plt.plot(predict_y, predict_y, 'ro')
+        plt.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=2)
+        plt.xlabel('Predicted')
+        plt.ylabel('Measured')
+        fig = plt.gcf()
+        fig.set_size_inches(10,6)
+        plt.gca().grid(True)
+
+        rv = StringIO()
+        plt.savefig(rv, format="svg")
+        return rv.getvalue()
+    finally:
+        plt.clf()
+
+def SVR_fitting(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat):
+    from django.conf import settings
+    import os
+    import numpy as np
+    from sklearn.externals import joblib
+
+    clf=joblib.load(os.path.join(settings.PROJECT_ROOT,'app','machine_SVR.pkl'))
+
+    if not crime:
+        my_variable =[0.02729,0,7.07,0,0.469,7.185,61.1,4.9671,2,242,17.8,39.283,9.14]
+        Y=clf.predict(np.array(my_variable))
+    else:
+        x = np.array([crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat], dtype=np.float64)
+        Y=clf.predict(x)
+
+    return Y
+
+def SVR_plot(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat):
+    from django.conf import settings
+    import os, matplotlib.pyplot as plt
+    from sklearn.externals import joblib
+    try:
+        from StringIO import StringIO
+    except ImportError:
+        from io import StringIO
+    from sklearn import datasets
+    from sklearn.cross_validation import cross_val_predict
+    import numpy as np
+
+    clf=joblib.load(os.path.join(settings.PROJECT_ROOT,'app','machine_SVR.pkl'))
+    boston = datasets.load_boston()
+    y = boston.target
+    Y=SVR_fitting(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat)
+
+    try:
+        predicted = clf.predict(boston.data)
+        predict_y=Y
+
+        plt.figure(1)
+        plt.clf()
+        plt.scatter(predicted,y,s=2)
+        plt.plot(predict_y, predict_y, 'ro')
+        plt.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=2)
+        plt.xlabel('Predicted')
+        plt.ylabel('Measured')
+        fig = plt.gcf()
+        fig.set_size_inches(10,6)
+        plt.gca().grid(True)
+
+        rv = StringIO()
+        plt.savefig(rv, format="svg")
+        return rv.getvalue()
+    finally:
+        plt.clf()
 
 
+def bokeh_plot(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat):
+    from django.conf import settings
+    import os
+    from sklearn.externals import joblib
+    from sklearn import datasets
+    from bokeh.plotting import figure, show, output_file
+    from bokeh.resources import CDN
+    from bokeh.embed import components
+
+    clf=joblib.load(os.path.join(settings.PROJECT_ROOT,'app','machine_SVR.pkl'))
+    boston = datasets.load_boston()
+    y = boston.target
+    Y=SVR_fitting(crime,zn,inidus,optradio,nox,rm,age,dis,rad,tax,ptratio,Bk,lstat)
 
 
-@login_required
-def list_MRAPP(request):
-    from app.models import MRAPP, file_log
-    """retrun restaurant list
+    predicted = clf.predict(boston.data)
+    predict_y=Y
+    p = figure(title = "Boston dataset")
+    p.xaxis.axis_label = 'Measured'
+    p.yaxis.axis_label = 'Predicted'
 
-    :request: client request
-    :returns: restaurant list webpage
-
-    """
-    MRAPPs = MRAPP.objects.all()
-    file_logs = file_log.objects.all()
-    #request.session['restaurants'] = restaurants
-    return render(request, 'app/list_MRAPP.html', {"MRAPPs":MRAPPs,
-        "file_logs":file_logs,})
-    #return render_to_response('app/list_MRAPP.html', locals())
-
-
-
+    p.scatter(y,predicted)
+    p.asterisk(x=predict_y, y=predict_y, size=20, color="#F0027F")
+    script, div = components(p, CDN)
+    return script, div
