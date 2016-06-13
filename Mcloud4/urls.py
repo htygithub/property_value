d74685b6d1543a16241ff10eaf15fd43839f128a
jean <jean1015jean1015@gmail.com>
2016-06-13 14:26:43 +0800

    update

--- Mcloud4/urls.py
+++ Mcloud4/urls.py
@@ -43,8 +43,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
-
-from Mcloud4 import settings
-urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
